a79148c0e11216f8f99574f4044836512a1f6367
matheussouza <souza_matheus@discente.ufg.br>
2020-12-17 13:55:08 -0300

    Inserindo configuração do banco e do insomnia

---
+++ database/Criação_tabelas_necessárias.sql
@@ -0,0 +1,31 @@
+GO
+CREATE TABLE `password_tokens` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `token` varchar(200) NOT NULL DEFAULT '0',
+  `user_id` int unsigned NOT NULL,
+  `used` tinyint unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+GO
+CREATE TABLE `produtos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(45) NOT NULL,
+  `tamanho` double NOT NULL,
+  `quantidade` int NOT NULL,
+  `valor` double NOT NULL,
+  `horario` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+)
+
+GO
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT '0',
+  `email` varchar(150) NOT NULL DEFAULT '0',
+  `password` varchar(200) NOT NULL DEFAULT '0',
+  `role` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+)
+
