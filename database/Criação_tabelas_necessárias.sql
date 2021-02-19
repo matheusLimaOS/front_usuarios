b79bea89f90e7793e6867df7a6ce253e70223c54
matholaslima <69645734+matholaslima@users.noreply.github.com>
2021-02-19 13:35:16 -0300

    Update Criação_tabelas_necessárias.sql

--- database/Criação_tabelas_necessárias.sql
+++ database/Criação_tabelas_necessárias.sql
@@ -1,4 +1,5 @@
-GO
+CREATE DATABASE `aula_node`;
+
 CREATE TABLE `password_tokens` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `token` varchar(200) NOT NULL DEFAULT '0',
@@ -6,9 +7,10 @@ CREATE TABLE `password_tokens` (
   `used` tinyint unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-)
-GO
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
+
+
 CREATE TABLE `produtos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `descricao` varchar(45) NOT NULL,
@@ -17,9 +19,8 @@ CREATE TABLE `produtos` (
   `valor` double NOT NULL,
   `horario` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-)
+);
 
-GO
 CREATE TABLE `users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT '0',
@@ -27,14 +28,34 @@ CREATE TABLE `users` (
   `password` varchar(200) NOT NULL DEFAULT '0',
   `role` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-)
-CREATE TABLE `aula_node`.`carrinho` (
-  `ID_carrinho` INT NOT NULL AUTO_INCREMENT,
-  `ID_produto` INT NOT NULL,
-  `descricao` VARCHAR(45) NOT NULL,
-  `valorprod` DOUBLE NOT NULL,
-  `quantprod` INT NOT NULL,
-  `usuario` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID_carrinho`));
+);
 
 
+CREATE TABLE `carrinho` (
+  `ID_carrinho` int NOT NULL AUTO_INCREMENT,
+  `ID_produto` int NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  `valorprod` double NOT NULL,
+  `quantprod` int NOT NULL,
+  `usuario` varchar(150) NOT NULL,
+  PRIMARY KEY (`ID_carrinho`)
+);
+
+CREATE TABLE `vendas_itens` (
+  `id_venda_item` int NOT NULL AUTO_INCREMENT,
+  `id_venda` int NOT NULL,
+  `id_produto` int NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  `quantidade_vendida` int NOT NULL,
+  `valor_vendido` double NOT NULL,
+  PRIMARY KEY (`id_venda_item`)
+);
+
+CREATE TABLE `vendas` (
+  `id_venda` int NOT NULL AUTO_INCREMENT,
+  `quantidade_vendida` int NOT NULL,
+  `valor_vendido` double NOT NULL,
+  `hora_venda` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_usuario` int NOT NULL,
+  PRIMARY KEY (`id_venda`)
+);
